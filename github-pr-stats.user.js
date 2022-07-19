1f2b672a3a91d5144582e720eebd5209bb2d75b3
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-19 02:22:58 -0700

    Fixing console log statement

--- github-pr-stats.user.js
+++ github-pr-stats.user.js
@@ -16,55 +16,60 @@
 // ==/UserScript==
 
 function loadDeveloperInsights() {
-    'use strict';
+  "use strict";
 
-    // True prints more debug information in the console; False prints minimal things in the console log.
-    const DEBUG = true;
+  // True prints more debug information in the console; False prints minimal things in the console log.
+  const DEBUG = true;
 
-    // The base URL for which to fetch data
-    const BASE_DATA_URL = "https://chriscarini.com/developer_insights/data.json";
+  // The base URL for which to fetch data
+  const BASE_DATA_URL = "https://chriscarini.com/developer_insights/data.json";
 
-    console.log("GitHub PR Stats starting...");
+  console.log("GitHub PR Stats starting...");
 
-    function debug(msg) {
-        if (DEBUG) {
-            console.log(msg);
-        }
+  function debug(msg) {
+    if (DEBUG) {
+      console.log(msg);
     }
+  }
 
-    debug("REQUEST URL: " + BASE_DATA_URL);
-    GM_xmlhttpRequest({
-        method: "GET", url: BASE_DATA_URL, onload: function (result) {
-            const data = JSON.parse(result.responseText);
-            debug(data);
+  debug("REQUEST URL: " + BASE_DATA_URL);
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: BASE_DATA_URL,
+    onload: function (result) {
+      const data = JSON.parse(result.responseText);
+      debug(data);
 
-            const metrics = data.metrics;
-            debug(metrics);
+      const metrics = data.metrics;
+      debug(metrics);
 
-            // Create element for us to use
-            const ourThing = document.createElement("div");
+      // Create element for us to use
+      const ourThing = document.createElement("div");
 
-            // TODO: Do some stuff with the metrics...
-            for (const [metricName, stats] of Object.entries(metrics)) {
-                debug(`Metric Name: ${metricName}`);
-                ourThing.innerHTML += `<hr/><div class="text-bold discussion-sidebar-heading discussion-sidebar-toggle hx_rsm-trigger">Metric Name: ${metricName}</div><br/>`;
+      // TODO: Do some stuff with the metrics...
+      for (const [metricName, stats] of Object.entries(metrics)) {
+        debug(`Metric Name: ${metricName}`);
+        ourThing.innerHTML += `<hr/><div class='text-bold discussion-sidebar-heading discussion-sidebar-toggle hx_rsm-trigger'>Metric Name: ${metricName}</div><br/>`;
 
-                debug(`Metric Stats:`);
-                for (const [statName, statValue] of Object.entries(stats)) {
-                    debug(`---> ${statName}: ${statValue}`);
-                    ourThing.innerHTML += `<span>${statName}: ${statValue['Overall']}</span><br/>`;
-                }
-            }
-
-            var theirThing = document.getElementById("partial-discussion-sidebar");
-            if (typeof theirThing === "undefined" || (typeof theirThing === "object" && theirThing === null)) {
-                theirThing = document.getElementsByClassName("BorderGrid--spacious")[0];
-            }
-            theirThing.prepend(ourThing)
+        debug(`Metric Stats:`);
+        for (const [statName, statValue] of Object.entries(stats)) {
+          debug(`---> ${statName}: ${statValue["Overall"]}`);
+          ourThing.innerHTML += `<span>${statName}: ${statValue["Overall"]}</span><br/>`;
         }
-    });
+      }
 
-    console.log("GitHub PR Stats completed.");
+      var theirThing = document.getElementById("partial-discussion-sidebar");
+      if (
+        typeof theirThing === "undefined" ||
+        (typeof theirThing === "object" && theirThing === null)
+      ) {
+        theirThing = document.getElementsByClassName("BorderGrid--spacious")[0];
+      }
+      theirThing.prepend(ourThing);
+    },
+  });
+
+  console.log("GitHub PR Stats completed.");
 }
 
 window.addEventListener("load", loadDeveloperInsights);
