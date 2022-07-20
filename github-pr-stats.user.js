89a39b473b0eebb1ca310a603b1ee5a8faeacb0d
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-20 01:18:49 -0700

    more userscript UI tweaks

--- github-pr-stats.user.js
+++ github-pr-stats.user.js
@@ -28,6 +28,10 @@ function debug(msg) {
     }
 }
 
+function toHrs(num) {
+    return (num / 3600).toFixed(2)
+}
+
 function loadDeveloperInsights() {
     'use strict';
 
@@ -43,40 +47,36 @@ function loadDeveloperInsights() {
             debug(metrics);
 
             // Create element for us to use
-            const ourThing = document.createElement("div");
-            ourThing.innerHTML += `<h4><b>p50/p90 (Biz Hours)</b></h4></br></div>`;
+            let ourHtml = `<h4><b>Code Review Metrics (biz hrs)</b></h4></div>`
 
             // TODO: Do some stuff with the metrics...
             for (const [metricName, stats] of Object.entries(metrics)) {
                 debug(`Metric Name: ${metricName}`);
                 debug(`Metric Stats:`);
-                ourThing.innerHTML += `<i>${metricName}:: </i>`;
+                ourHtml += `<div style="display: flex; justify-content: space-between;"><b>${metricName}:</b>`;
                 for (const [statName, statValue] of Object.entries(stats)) {
                     debug(`---> ${statName}: ${statValue['Overall']}`);
-                    ourThing.innerHTML += `<span>${(statValue['Overall']/3600).toFixed(2)}</span>`;
-                    if(statName !== 'P90') {
-                       ourThing.innerHTML += `<span></span>/`;
-                    }
+                    ourHtml += `<div>${statName}: ${toHrs(statValue['Overall'])}</div>`;
                 }
-                ourThing.innerHTML += `</br>`;
-                for (const [statName, statValue] of Object.entries(stats)) {
-                    ourThing.innerHTML += `<details>
-                      <summary>${statName} Details</summary>
-                      <table>
-                        <tr><th>Size</th><th>Biz Hrs</th></tr>
-                        <tr><td style='color:MediumSeaGreen;'>Small</td><th>${(statValue['Small']/3600).toFixed(2)}</th></tr>
-                        <tr><td style='color:DodgerBlue;'>Medium</td><th>${(statValue['Medium']/3600).toFixed(2)}</th></tr>
-                        <tr><td style='color:Orange;'>Large</td><th>${(statValue['Large']/3600).toFixed(2)}</th></tr>
-                        <tr><td style='color:Tomato;'>XL</td><th>${(statValue['X-Large']/3600).toFixed(2)}</th></tr>
-                      </table>
-                    </details>`;
-                }
-                ourThing.innerHTML += `</br>`;
+                ourHtml += `</div>`;
+                ourHtml += `<details><summary><i>Code Size Breakdown</i></summary>
+                  <table style='padding: 10px; margin: 10px; width: 95%;'>
+                    <tr style="background-color: var(--color-canvas-subtle);"><th>Size</th><th>P50 Biz Hrs</th><th>P90 Biz Hrs</th></tr>
+                    <tr><td style='text-align: center; color:MediumSeaGreen;'>Small</td><th>${toHrs(stats['P50']['Small'])}</th><th>${toHrs(stats['P90']['Small'])}</th></tr>
+                    <tr><td style='text-align: center; color:DodgerBlue;'>Medium</td><th>${toHrs(stats['P50']['Medium'])}</th><th>${toHrs(stats['P90']['Medium'])}</th></tr>
+                    <tr><td style='text-align: center; color:Orange;'>Large</td><th>${toHrs(stats['P50']['Large'])}</th><th>${toHrs(stats['P90']['Large'])}</th></tr>
+                    <tr><td style='text-align: center; color:Tomato;'>XL</td><th>${toHrs(stats['P50']['X-Large'])}</th><th>${toHrs(stats['P90']['X-Large'])}</th></tr>
+                  </table></details>`;
+                ourHtml += `</br>`;
             }
-            var theirThing = document.getElementById("partial-discussion-sidebar");
+            let theirThing = document.getElementById("partial-discussion-sidebar");
             if (typeof theirThing === "undefined" || (typeof theirThing === "object" && theirThing === null)) {
                 theirThing = document.getElementsByClassName("BorderGrid--spacious")[0];
             }
+
+            const ourThing = document.createElement("div");
+            ourThing.innerHTML = ourHtml
+
             theirThing.prepend(ourThing)
         }
     });
