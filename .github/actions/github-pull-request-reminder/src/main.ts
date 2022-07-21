fe9dcb71f1973b22084893ccfc189a3785df569b
Ankur Mehta <ankurmehtamail@gmail.com>
2022-07-21 16:30:21 -0700

    added details to pr comment

--- .github/actions/github-pull-request-reminder/src/main.ts
+++ .github/actions/github-pull-request-reminder/src/main.ts
@@ -9,6 +9,7 @@ const octokit = github.getOctokit(myToken)
 const owner: string = github.context.repo.owner
 const repo: string = github.context.repo.repo
 const reminder_seconds = 15 * 60
+const seconds_in_hour = 3600
 
 async function pullRequests(repoOwner: string, repoName: string): Promise<RestEndpointMethodTypes['pulls']['list']['response']> {
   return octokit.rest.pulls.list({
@@ -53,6 +54,8 @@ async function run(): Promise<void> {
       const current_time = new Date()
       const pr_creation_time = new Date(pr.created_at)
       const age_seconds = (current_time.getTime() - pr_creation_time.getTime()) / 1000
+      const age_hours = age_seconds / seconds_in_hour
+      const crl_hours = crl_p50 / seconds_in_hour
       let reviewer_mention = ''
       for (const login of reviewerLogins.split(', ')) {
         if (login !== '') {
@@ -60,15 +63,20 @@ async function run(): Promise<void> {
         }
       }
 
-      const comment = 'Please review this pull request to reduce the P50 code review latency for this multiproduct.'
-      core.info(`Overall p50 CRL:  ${crl_p50}`)
+      const comment = `Hi ${reviewer_mention}
+  
+`@${pr.user?.login}` opened this PR ${age_hours} business hours ago, and the P50 code review latency for this MP is ${crl_hours} business hours. If you are able, review this code now to help reduce this multiproduct's Code Review Latency!
+
+Beep Boop Beep,
+GitHub PR Reminder Bot`
+
       core.info(`Time since creation: ${age_seconds}`)
       const list_params = {owner, repo, issue_number: pr.number}
       octokit.rest.issues.listComments(list_params).then(comments => {
-        const index = comments.data.findIndex(comments => comments.body?.includes(comment))
+        const index = comments.data.findIndex(comments => comments.body?.includes("GitHub PR Reminder Bot"))
         if (index === -1) {
           core.info(`Needs reminder`)
-          if (age_seconds >= crl_p50 - reminder_seconds) {
+          if (age_seconds >= crl_p50 - reminder_seconds && age_seconds < crl_p50) {
             //comment when time since creation is within reminder time of the p50 crl
             const create_params = {
               owner: owner as string,
