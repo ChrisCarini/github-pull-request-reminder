83c6caab0191ccb22c81b764861bc194bb42bc50
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-22 02:02:58 -0700

    Restructuring project into multiple actions at the top level + fixing ESLint suggestions

--- src/summary/main.ts
+++ src/summary/main.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
-import * as fs from 'fs'
+import {getMetrics, pullRequests} from '../lib'
 
 const myToken = core.getInput('GITHUB_TOKEN')
 const octokit = github.getOctokit(myToken)
@@ -9,39 +8,23 @@ const octokit = github.getOctokit(myToken)
 const owner: string = github.context.repo.owner
 const repo: string = github.context.repo.repo
 
-async function closedPullRequests(repoOwner: string, repoName: string): Promise<RestEndpointMethodTypes['pulls']['list']['response']> {
-  return octokit.rest.pulls.list({
-    owner: repoOwner,
-    repo: repoName,
-    state: 'closed',
-    sort: 'created'
-  })
-}
-
-async function getMetrics(): Promise<any> {
-  const response = fs.readFileSync('./data.json', 'utf-8')
-  const result = JSON.parse(response) as any
-  return result
-}
-
 async function run(): Promise<void> {
   try {
-    const allClosedPrs = await closedPullRequests(owner, repo)
+    const allClosedPrs = await pullRequests(octokit, owner, repo, 'closed')
     for (const pr of allClosedPrs.data) {
       core.debug(`PR: ${JSON.stringify(pr, null, 2)}`)
 
-      if(pr.merged_at) {
-
+      if (pr.merged_at) {
         const merged_time = new Date(pr.merged_at)
         const creation_time = new Date(pr.created_at)
         const age_seconds = (merged_time.getTime() - creation_time.getTime()) / 1000
         const age = age_seconds / 3600
-        const api_response = await getMetrics()
-        const ttm_p50 = api_response.metrics['Time to Merge'].P50.Overall
+        const data = getMetrics()
+        const ttm_p50 = data.metrics['Time to Merge'].P50.Overall
         const percent_diff = (100 * Math.abs((age - ttm_p50) / ttm_p50)).toFixed(2)
-        const direction =  age_seconds > ttm_p50 ? "higher" : "lower"
+        const direction = age_seconds > ttm_p50 ? 'higher' : 'lower'
 
-        const comment = `Hi @${pr.user?.login}
+        const commentText = `Hi @${pr.user?.login}
   
 Here is a summary of your pull request:
 
@@ -52,13 +35,13 @@ GitHub PR Metrics Bot`
 
         const list_params = {owner, repo, issue_number: pr.number}
         octokit.rest.issues.listComments(list_params).then(comments => {
-          const index = comments.data.findIndex(comments => comments.body?.includes("GitHub PR Metrics Bot"))
+          const index = comments.data.findIndex(comment => comment.body?.includes('GitHub PR Metrics Bot'))
           if (index === -1) {
             const create_params = {
-              owner: owner as string,
-              repo: repo as string,
-              issue_number: pr.number as number,
-              body: comment as string
+              owner,
+              repo,
+              issue_number: pr.number,
+              body: commentText
             }
             octokit.rest.issues.createComment(create_params)
           }
