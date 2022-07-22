ca9482890af007b6ffcb6ad59f88f87447b30a07
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-22 02:02:58 -0700

    Restructuring project into multiple actions at the top level + fixing ESLint suggestions

---
+++ src/lib/data.ts
@@ -0,0 +1,36 @@
+import fs from 'fs'
+
+export interface MetricsData {
+  metrics: Metrics;
+  thresholds: Threshold[];
+}
+
+interface Metrics {
+  'Code Review Latency': Percentiles;
+  'Time to Approval': Percentiles;
+  'Time to Merge': Percentiles;
+}
+
+interface Percentiles {
+  P50: Sizes;
+  P90: Sizes;
+}
+
+interface Sizes {
+  Large: number;
+  Medium: number;
+  Overall: number;
+  Small: number;
+  'X-Large': number;
+}
+
+interface Threshold {
+  name: string;
+  threshold: number;
+}
+
+
+export function getMetrics(): MetricsData {
+  const response = fs.readFileSync('./data.json', 'utf-8')
+  return JSON.parse(response)
+}
