8e34ad031d627671f08fbfbe07835112462f0b08
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2022-07-22 01:31:29 -0700

    actions use local file, curl'd in workflow

--- .github/actions/github-pull-request-metrics/src/main.ts
+++ .github/actions/github-pull-request-metrics/src/main.ts
@@ -19,7 +19,7 @@ async function closedPullRequests(repoOwner: string, repoName: string): Promise<
 }
 
 async function getMetrics(): Promise<any> {
-  const url = 'https://chriscarini.com/developer_insights/data.json'
+  const url = './data.json'
   const response = await fetch(url)
   const result = (await response.json()) as any
   return result
